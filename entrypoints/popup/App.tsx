27c01d70be159787593d987029241b2bde4e5a1c
yeasin2002 <mdkawsarislam2002@gmail.com>
2024-07-24 18:09:55 +0600

    feat: added tabs listing,adding and removing

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,14 +1,43 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [allTabs, setTabs] = useState<chrome.tabs.Tab[]>([]);
+
+  useEffect(() => {
+    chrome.tabs.query({}, function (tabs) {
+      setTabs(tabs);
+    });
+  }, []);
+
+  const handleClose = (tabId: number) => {
+    chrome.tabs.remove(tabId, () => {
+      setTabs((prevTabs) => prevTabs.filter((tab) => tab.id !== tabId));
+    });
+  };
+
   return (
     <div id="app">
       <h1 className="main-heading">Tab Manager </h1>
-
-      <div className="tab-items-wrapper">
-        <p>Tab Name</p>
-        <button className="close-btn">Close</button>
+      <div className="top-btn-wrapper">
+        <button className="add-btn" onClick={() => chrome.tabs.create({})}>
+          New Tab
+        </button>
       </div>
+
+      {allTabs.map((tabItems) => {
+        return (
+          <div className="tab-items-wrapper">
+            <p>{tabItems.title}</p>
+            <button
+              className="close-btn"
+              onClick={() => handleClose(tabItems.id!)}
+            >
+              Close
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 }
